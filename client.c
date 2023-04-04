0503a55f44790e47b76c994369f67d93a44dcc4e
Abde-Ljalil Ez-Zirani <abez-zir@student.1337.ma>
2023-04-04 02:54:43 +0000

    Updating ba9i lblan deyaal ctrl c

--- client.c
+++ client.c
@@ -20,149 +20,35 @@
 #include <sys/types.h>
 
 
-static void	ft_bit_sender(int pid, char chara)
-{
-	int	bit;
-
-	bit = 0;
-	while (bit < 8)
-	{
-		if ((chara & (1 << bit)) != 0)
-		{
-			if (kill(pid, SIGUSR1) == -1)
-			{
-				write (2, "\e[033;0;31m→  PID not correct. ❌\e[0m\n", 47);
-				exit(0);
-			}
-		}
-		else
-		{
-			if (kill(pid, SIGUSR2) == -1)
-			{
-				write (2, "\e[033;0;31m→  PID not correct. ❌\e[0m\n", 47);
-				exit(0);
-			}
-		}
-		usleep(800);
-		bit++;
-	}
-}
-
-void	ft_send_message(int pid, char *msg)
-{
-	int	i;
-
-	i = 0;
-	while (msg[i])
-	{
-		ft_bit_sender(pid, msg[i]);
-		i++;
-	}
-	ft_bit_sender(pid, '\n');
-	ft_bit_sender(pid, '\0');
-}
-
-
 int main (int argc, char *argv[])
 {
-    int i;
-    int j;
+	int i;
+	int j;
 
-    i = 0;
-    j = 0;
-    if (argc == 3)
-    {
-        // while (argv[2][j])
-        // {
-        //     while (i < 8)
-        //     {
-        //         if (((argv[2][j] >> i) & 1) == 1)
-        //         {
-        //             if (kill(pid, SIGUSR1) == -1)
-        //                 return (0);
-        //         }
-        //         else
-        //         {
-        //             if (kill(pid, SIGUSR2) == -1)
-        //                 return (0);
-        //         }
-        //         usleep(700);
-        //         i++;
-        //     }
-        //     j++;
-        int pid = atoi(argv[1]);
-        ft_send_message(pid, argv[2]);
-        // }
-    }
+	i = 0;
+	j = 0;
+	int pid = atoi(argv[1]);
+	if (argc == 3)
+	{
+	    while (argv[2][j])
+	    {
+	        while (i <= 7)
+	        {
+	            if (((argv[2][j] >> i) & 1) == 1)
+	            {
+	                if (kill(pid, SIGUSR1) == -1)
+	                    return (0);
+	            }
+	            else
+	            {
+	                if (kill(pid, SIGUSR2) == -1)
+	                    return (0);
+	            }
+	            usleep(600);
+	            i++;
+	        }
+	        j++;
+			i = 0;
+	    }
+	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // char c = 'a';
-    // int i;
-
-
-    // i = 0;
-
-
-    // while (i < 8)
-    // {
-    //     if (((c << 1) & 1) == 1)
-    //     {
-    //         kill(pid, SIGUSR1);
-    //     }
-    //     else
-    //     {
-    //         kill(pid, SIGUSR2);
-    //     }
-    //     usleep(700);
-    //     i++;
-    // }
