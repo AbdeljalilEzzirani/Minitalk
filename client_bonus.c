8cf458648ddd40a7885ba69c5469d209ff9e4d1a
Abde-Ljalil Ez-Zirani <abez-zir@student.1337.ma>
2023-04-05 18:16:15 +0000

    Updating kandoon mandatori salaaat

--- client_bonus.c
+++ client_bonus.c
@@ -10,4 +10,52 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
+#include <sys/wait.h>
+#include <signal.h>
+#include <sys/types.h>
+
+int	function(char ch, int pid)
+{
+	int				i;
+
+	while (i <= 7)
+	{
+		if (((ch >> i) & 1) == 1)
+		{
+			if (kill(pid, SIGUSR1) == -1)
+				return (0);
+		}
+		else
+		{
+			if (kill(pid, SIGUSR2) == -1)
+				return (0);
+		}
+		usleep(600);
+		i++;
+	}
+	i = 0;
+	return (0);
+}
+
+int	main(int argc, char *argv[])
+{
+	int				j;
+	int				pid;
+
+	pid = atoi(argv[1]);
+	j = 0;
+	if (argc == 3)
+	{
+		while (argv[2][j])
+		{
+			function(argv[2][j], pid);
+			j++;
+		}
+	}
+	return (0);
+}
--- server_bonus.c
+++ server_bonus.c
@@ -10,4 +10,53 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <limits.h>
 
+void	ft_handler(int num, siginfo_t *siginfo, void *c)
+{
+	static unsigned char								l;
+	static int								i = 0;
+	static int								pid = 0;
+
+	(void)c;
+	if (pid != (siginfo->si_pid))
+	{
+		pid = siginfo->si_pid;
+		l = 0;
+		i = 0;
+	}
+	if (num == SIGUSR1)
+		l = l + (1 << i);
+	i++;
+	if (i == 8)
+	{
+		write(1, &l, 1);
+		i = 0;
+		l = 0;
+	}
+}
+
+int	main(int argc, char *argv[])
+{
+	int						pid;
+	struct sigaction		sa;
+
+	pid = getpid();
+	printf("%d\n", pid);
+	sa.sa_sigaction = ft_handler;
+	sa.sa_flags = SA_SIGINFO;
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
+	if (sigaction)
+		kill(pid, SIGUSR2);
+	printf("Waiting for signal...\n");
+	while (1)
+		pause();
+}
